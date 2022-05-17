c791474672a6673f85aebb92733270516bc031d0
areyes403 <areyes403>
2022-05-17 09:52:50 -0500

    First Commit

--- ListaTareas/ViewController.swift
+++ ListaTareas/ViewController.swift
@@ -7,11 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+    
+    
+    
+    @IBOutlet weak var tableNotas: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableNotas.delegate=self
+        tableNotas.dataSource=self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tableNotas.dequeueReusableCell(withIdentifier: "celda", for: indexPath)
+        celda.textLabel?.text="testing"
+        celda.detailTextLabel?.text="subtesting"
+        
+        return celda
     }
 
 
